8fe242b8558e7b37b86d06294077b0b766d9615d
Alex5727 <alejandrogrjalva@gmail.com>
2024-11-23 01:03:00 -0700

    Practica 2.10

---
+++ StatuBar_JAGS/StatuBar_JAGS/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using StatuBar_JAGS.VistaModelo;
+
+namespace StatuBar_JAGS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new VMmainpage();
+        }
+    }
+}
---
+++ StatuBar_JAGS/StatuBar_JAGS/VistaModelo/VMstatusbar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatuBar_JAGS.VistaModelo
+{
+    public interface VMstatusbar
+    {
+        void OcultarStatusBar();
+
+        void MostrarStatusBar();
+
+        void Traslucido();
+
+        void Transparente();
+
+        void CambiarColor();
+    }
+}
